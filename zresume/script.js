ecf3ca2dff09c81846d083e2e6b329bf1ed3fd0e
asadshakri <asadshakri3127@gmail.com>
2025-08-27 23:45:55 +0530

    done!

---
+++ zresume/script.js
@@ -0,0 +1,64 @@
+/*function buyBike(planTrip) {
+    setTimeout(function(){ console.log("Bought Royal Enfield Himalayan"); planTrip()}, 2000);
+  }
+  
+  function planTrip() {
+    setTimeout(() => { console.log("Trip to Ladakh") }, 1000);
+  }
+  buyBike(planTrip);*/
+
+ /* const projectorPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const rented = false;
+        if (rented)
+            resolve("Projector rented successfully");
+        else
+            reject("Error: Projectors out of stock");
+    },1000)
+});
+
+const pizzasPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const rented = false;
+        if (rented)
+            resolve("Pizzas delivered at time");
+        else
+            reject("Error: Pizzas not delivered on time");
+    },2000)
+});
+
+const friendsPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const rented = false;
+        if (rented)
+            resolve("Both friends available");
+        else
+            reject("Error: Both friends not available");
+    },3000)
+});
+
+Promise.all([projectorPromise, pizzasPromise, friendsPromise]).then((result) => {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+})*/
+
+async function picnicDecision() {
+  // Write your code here
+  
+    
+      const checkWeather= await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const isSunny = false;
+          if (isSunny)
+            resolve("Let us go for picnic");
+          else
+            reject("Error: It is cloudy");
+        },3000)
+      })
+       console.log(checkWeather);
+       console.log("Picnic started");
+  
+}
+
+picnicDecision();
